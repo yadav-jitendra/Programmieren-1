64884633ecd6c0cb32c250c57b54f31c5584ba21
Jitendra Kumar Yadav <j.yadav@tu-braunschweig.de>
2021-03-28 03:43:08 +0200

    update

--- src/blatt02/Haeufigkeit.java
+++ src/blatt02/Haeufigkeit.java
@@ -10,38 +10,38 @@ public class Haeufigkeit {
     public static void main(String[] args) {
         int rows = 256;
         int col = 2;
-        int [][] array = new int[rows][col];
+        int[][] array = new int[rows][col];
 
         File file = new File(FILE_NAME);
         Scanner fileReader = null;
         try {
-           fileReader = new Scanner(file);
+            fileReader = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         int rowCounter = 0;
-        while (fileReader.hasNextLine()){
+        while (fileReader.hasNextLine()) {
             String line = fileReader.nextLine();
-            for (char symbol : line.toCharArray()){
-                if (!symbolInArray(array, symbol)){
+            for (char symbol : line.toCharArray()) {
+                if (!symbolInArray(array, symbol)) {
                     array[rowCounter][0] = symbol;
                     array[rowCounter][1]++;
                     rowCounter++;
-                }else {
+                } else {
                     int rowNumber = findSymbol(array, symbol);
                     array[rowNumber][1]++;
                 }
             }
         }
 
-//        sortArray(array);
+        sortArray(array);
         printArray(array);
 
     }
 
     private static boolean symbolInArray(int[][] array, char symbol) {
         for (int i = 0; i < 256; i++) {
-            if (array[i][0] == symbol){
+            if (array[i][0] == symbol) {
                 return true;
             }
         }
@@ -51,25 +51,38 @@ public class Haeufigkeit {
     private static int findSymbol(int[][] array, char symbol) {
         int rowNumber = -1;     // -1 means not in the Array
         for (int i = 0; i < 256; i++) {
-            if (array[i][0] == symbol){
+            if (array[i][0] == symbol) {
                 return i;
             }
         }
         return rowNumber;
     }
 
-    private static void sortArray(int[][] array) {
-        // sort array in descending order
-    }
-
-    private static void printArray(int[][] array) {
-        for (int i = 0; i < 256; i++) {
-            if (!(array[i][0] == 0 && array[i][1] == 0)){
-                System.out.println((char)array[i][0] + " : " + array[i][1]);
+    private static void sortArray(int[][] array) {   //in descending order of frequency
+        int[] temp;
+        boolean sorted = true;
+        int j = 0;
+        while (sorted) {
+            sorted = false;
+            j++;
+            for (int i = 0; i < array.length - j; i++) {
+                if (array[i][1] < array[i + 1][1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    sorted = true;
+                }
             }
         }
     }
 
+    private static void printArray(int[][] array) {
+        for (int i = 0; i < 256; i++) {
+            if ((array[i][0] != 0 && array[i][1] != 0)) {
+                System.out.println((char) array[i][0] + " : " + array[i][1]);
+            }
+        }
+    }
 
 
 }
